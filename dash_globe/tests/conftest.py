19754c515702e8253b4e331956c5272e9df52568
jeffgallini <52107906+jeffgallini@users.noreply.github.com>
2026-04-18 14:20:42 -0500

    Create conftest.py

---
+++ dash_globe/tests/conftest.py
@@ -0,0 +1,11 @@
+from selenium.webdriver import ChromeOptions
+
+
+def pytest_setup_options():
+    """Provide CI-stable Chrome options for dash[testing] browser fixtures."""
+    options = ChromeOptions()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
+    return options
